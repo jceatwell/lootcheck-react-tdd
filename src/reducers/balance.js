7e71d5e9c97969082ce38ef84d6dbbaee456a646
John Eatwell <jceatwell@gmail.com>
2021-05-01 22:52:02 +0200

    feat: add cookie support
    Library used is sfcookies

--- src/reducers/balance.js
+++ src/reducers/balance.js
@@ -1,16 +1,31 @@
+import { bake_cookie, read_cookie } from 'sfcookies';
 import * as constants from '../actions/constants';
 
+// Read_cookie takes in a string as which cookie you want to read,
+// bake_cookie takes in a string as first argument, identifying which cookie you want to save
+// The second parameter is the item you want to store
+
+const BALANCE_COOKIE = 'BALANCE_COOKIE';
+
 const balance = (state = 0, action) => {
+    let balance;
+
     switch (action.type) {
         case constants.SET_BALANCE:
-            return action.balance;
+            balance = action.balance;
+            break;
         case constants.DEPOSIT:
-            return state + action.deposit;
+            balance = state + action.deposit;
+            break;
         case constants.WITHDRAW:
-            return state - action.withdrawal;
+            balance = state - action.withdrawal;
+            break;
         default:
-            return state;
+            balance = parseInt(read_cookie(BALANCE_COOKIE), 10) || state;
     }
+
+    bake_cookie(BALANCE_COOKIE, balance);
+    return balance;
 };
 
 export default balance;
--- src/reducers/balance.test.js
+++ src/reducers/balance.test.js
@@ -1,19 +1,28 @@
 import * as constants from '../actions/constants';
-import balanceReducer from './balance';
+import { default as balanceReducer, default as balanceReducer2 } from './balance';
 
 describe('balanceReducer', () => {
-    it('set a balance', () => {
+    describe('when initializing', () => {
         const balance = 10;
-        // balanceReducer(prevState, action)
-        expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
-            .toEqual(balance);
+
+        it('set a balance', () => {
+            // balanceReducer(prevState, action)
+            expect(balanceReducer(undefined, { type: constants.SET_BALANCE, balance }))
+                .toEqual(balance);
+        });
+
+        describe('then re-initialize', () => {
+            it('reads the balance from cookies', () => {
+                expect(balanceReducer2(undefined, {})).toEqual(balance);
+            });
+        });
     });
 
     it('deposits into the balance', () => {
         const deposit = 10;
         const initialState = 5;
 
-        expect(balanceReducer(initialState, {type: constants.DEPOSIT, deposit}))
+        expect(balanceReducer(initialState, { type: constants.DEPOSIT, deposit }))
             .toEqual(initialState + deposit);
     });
 
@@ -21,7 +30,7 @@ describe('balanceReducer', () => {
         const withdrawal = 10;
         const initialState = 20;
 
-        expect(balanceReducer(initialState, {type: constants.WITHDRAW, withdrawal}))
+        expect(balanceReducer(initialState, { type: constants.WITHDRAW, withdrawal }))
             .toEqual(initialState - withdrawal);
     });
 });
